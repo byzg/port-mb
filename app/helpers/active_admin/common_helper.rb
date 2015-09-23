12c7aaf4f839d21c71d9137db3cc84a57ed229ec
byzg <byzg@mail.ru>
2015-09-23 18:27:18 +0300

    decorate index album path 1.2

--- app/helpers/active_admin/common_helper.rb
+++ app/helpers/active_admin/common_helper.rb
@@ -1,29 +1,30 @@
-module ActiveAdmin::CommonHelper 
+module ActiveAdmin::CommonHelper
 
   def albums_wrapped_list(scope = @hierarchy, deep = '', list = [])
-    # Rails.logger.info '##################'
-    # Rails.logger.info scope
-    # scope.each do |album, hierarchy|
-    #   list << {name: (deep + (album.name || "##{album.id}")),
-    #            value: album.id,
-    #            deepest: hierarchy.empty?
-    #           }
-    #   list += albums_wrapped_list(hierarchy, deep + '-')
-    # end
-    # list
+    scope.each do |album, hierarchy|
+      list << {name: (deep + (album.name || "##{album.id}")),
+               value: album.id,
+               deepest: hierarchy.empty?
+              }
+      list += albums_wrapped_list(hierarchy, deep + '-')
+    end
+    list
   end
 
   def albumable_select mresource
-    # @albumable_select ||= select :resource, :album_id, {include_blank: true}, {class: 'pull-left'} do
-    #   albums_wrapped_list.each do |option|
-    #     option_params = {value: option[:value]}
-    #     case mresource.class
-    #       when Photo
-    #         option_params[:disabled] = option[:deepest]
-    #     end
-    #     content_tag(:option, option[:name], option_params)
-    #   end
-    # end
+    unless @albumable_select
+      options = (albums_wrapped_list.map do |option|
+        option_params = {value: option[:value]}
+        case mresource.class
+          when Photo
+            option_params[:disabled] = option[:deepest]
+        end
+        content_tag(:option, option[:name], option_params)
+      end).join('')
+      p options
+      @albumable_select = select_tag :resource, options.html_safe
+    end
+    @albumable_select
   end
 
 end
